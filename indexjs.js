b700cace82de12bef5a6fb6de0687f621e1988a4
Faritha Nasreen <faritha@freshdesk.com>
2016-06-28 21:32:47 +0530

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -1,9 +1,11 @@
 var socket = io.connect('http://localhost:3000');
 var height = 0;
+var clientname;
 // on connection to server, ask for user's name with an anonymous callback
 socket.on('connect', function(){
     // call the server-side function 'adduser' and send one parameter (value of prompt)
-    socket.emit('adduser', prompt("What's your name?"));
+    clientname = prompt("What's your name?");
+    socket.emit('adduser', clientname );
 });
 
 // listener, whenever the server emits 'updatechat', this updates the chat body
@@ -14,27 +16,26 @@ socket.on('updatechat', function (username, data) {
 
 // listener, whenever the server emits 'updaterooms', this updates the room the client is in
 socket.on('updaterooms', function(rooms, current_room) {
-    
+    //alert(current_room.id);
     $('#rooms').empty();
     $.each(rooms, function(key, value) {
-            
-        if(value == current_room){
-            $('#rooms').append('<div><a href="#" class="selected">' + value + '</a></div>');
-        } else {
-            $('#rooms').append('<div><a href="#" onclick="switchRoom(\''+ value +'\')">' + value + '</a></div>');
+           //alert(value.id);
+           //alert(current_room.id); 
+        if(value.id == current_room.id){
+            $('#rooms').append('<div><a href="#" class="selected">' + value.id + '</a></div>');
+        } 
+        else{
+            $('#rooms').append('<div><a href="#" onclick="switchRoom(\''+ value.id +'\')">' + value.id + '</a></div>');
         }
     });
 });
 
-socket.on('updateroomcount', function(newroom){
-    $('#rooms').append('<div><a href="#" onclick="switchRoom(\''+ newroom +'\')">' + newroom + '</a></div>');
+socket.on('updateRoomCount', function(value){
+    $('#rooms').append('<div><a href="#" onclick="switchRoom(\''+ value.id +'\')">' + value.id + '</a></div>');
 });
 
-function switchRoom(room){
-    socket.emit('switchRoom', room);
-}
-
 socket.on('updategame', function (id, v) {
+    //alert(id);
     $('#'+id).text(v);
 });
 
@@ -53,9 +54,19 @@ socket.on('restartgame', function(){
     }
 });
 
-// on load of page
+socket.on('refreshGame', function(room){
+    for ( var i = 0; i < 9; i++){
+        $('#'+i).text('');
+    }
+    for ( var i = 0; i < 9; i++){
+        if(room.arrX[i] != '')
+        $('#'+i).text('x');
+        if(room.arrO[i] != '')
+        $('#'+i).text('o');
+    }
+});
+
 $(function(){
-    // when the client clicks SEND
     $('#datasend').click( function() {
         
         var message = $('#data').val();
@@ -63,7 +74,6 @@ $(function(){
         socket.emit('sendchat', message );
     });
 
-    // when the client hits ENTER on their keyboard
     $('#data').keypress(function(e) {
         if(e.which == 13) {
             $(this).blur();
@@ -74,23 +84,24 @@ $(function(){
         height += parseInt($(this).height());
     });
     height += '';
+
 });
 
 function change(id){
-    socket.emit('changeValue', id);
+    socket.emit('changeValue', id, clientname);
 }
 
 function startnew(){
-    socket.emit('startover');
+    location.reload();
 }
 
-function createroom(){
+function createRoom(){
     socket.emit('createNewRoom');
 }
 
-
-
-
+function switchRoom(roomid){
+    socket.emit('switchRoom',roomid, clientname);
+}
 
 
 
